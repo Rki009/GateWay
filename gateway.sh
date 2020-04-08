5672ce9f2aa0f5f34845792ef847e97faf0b7271
Rki009 <33234231+Rki009@users.noreply.github.com>
2020-04-08 18:55:09 +0700

    Update gateway.sh

--- gateway.sh
+++ gateway.sh
@@ -1,45 +1,69 @@
 #!/bin/bash
 #	(C) Ron K. Irvine, 2020. All rights reserved.
+# gateway.sh
+#	Script to monitor the Raspberry Pi's WiFi Internet gateway.
+#	Red LED:
+#		Off	- trurned off
+#	Green LED:
+#		Off	- No Internet Gateway
+#		On	- Gateway detected, daytime hours
+#		Blink	- Gateway detected, night hours 
+#
 
-# daylight - daylight hours; 8:00 to 11pm
+# daytime - daytime hours; 07:30 to 23:00
+# don't foget the leading 0 on the hour "hh:mm", 5 characters
 # note sting compare is limited to EQ, GT and LT
+# so LE (less than or equal) is the same as NOT GT (not greater than)
 isDaylight() {
     currenttime=$(date +%H:%M)
-    if [[ "$currenttime" > "07:59" ]] && [[ "$currenttime" < "23:00" ]]; then
-        daylight=1
+    if [[ ! "$currenttime" < "07:30" ]] && [[ "$currenttime" < "23:00" ]]; then
+        daytime=1
     else
-        daylight=0
+        daytime=0
     fi
-    printf "$currenttime: Daylight = $daylight\n"
+    printf "$currenttime: Daytime = $daytime\n"
 }
 
 
-# get the gateway's IP address; empty string if no gateway
-gateway=`ip r | grep default | cut -d ' ' -f 3`
+# use the 'ip route' command to get the gateway's IP address
+#	an empty string if no gateway
+gateway=`ip route | grep "default via" | cut -d ' ' -f 3`
+device=`ip route | grep "default via" | cut -d ' ' -f 5`
 if [ -z "$gateway" ]; then
 	printf "Gateway: Not Found...\n"
 else
-	printf "Ping Gateway: $gateway\n"
+	printf "Ping Gateway: $gateway - $device\n"
 	ping -q -w 1 -c 1 $gateway >/dev/null 2>/dev/null && echo Gateway - Ok! || echo Gateway - ERROR!
 fi
 
+led0=/sys/class/leds/led0
+led1=/sys/class/leds/led1
 # disable [mmc0] trigger (control) for Green LED
-sudo bash -c "echo none >/sys/class/leds/led0/trigger"
+sudo bash -c "echo none >$led0/trigger"
 # disable Red power LED - 0 = Off
-sudo bash -c "echo 0 >/sys/class/leds/led1/brightness"
+sudo bash -c "echo 0 >$led1/brightness"
 
 printf "Loop forever ... ^C to exit\n"
 while [ 1 ]; do
 	isDaylight
-	gateway=`ip r | grep default | cut -d ' ' -f 3`
+	gateway=`ip route | grep "default via" | cut -d ' ' -f 3`
 	if [ -z "$gateway" ]; then
 		printf "Gateway - Down!\n"
 		# Green LED - 0 = Off
-		sudo bash -c "echo 0 >/sys/class/leds/led0/brightness"
+		sudo bash -c "echo 0 >$led0/brightness"
 	else
-		printf "Gateway - Ok!\n"
+		printf "Gateway $gateway - Ok!\n"
 		# Green LED - 1 = On
-		sudo bash -c "echo $daylight >/sys/class/leds/led0/brightness"
+		# sudo bash -c "echo $daytime >$led0/brightness"
+		if [ "$daytime" -eq "1" ]; then
+			# daytime - GREEN LED - On
+			sudo bash -c "echo 1 >$led0/brightness"
+		else
+			# at night - just a short blink of the GREEN LED
+			sudo bash -c "echo 1 >$led0/brightness"
+			sleep 0.001
+			sudo bash -c "echo 0 >$led0/brightness"
+ 		fi
 	fi
 	sleep 3
 done
